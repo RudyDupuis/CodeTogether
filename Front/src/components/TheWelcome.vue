1760f64b85d065d48af2988a9fe0e9ee92ca9fa2
RudyDupuis <abcdupuisrudy@gmail.com>
2024-03-15 20:54:53 +0100

    move vite plugin vuetify

--- Front/src/components/TheWelcome.vue
+++ Front/src/components/TheWelcome.vue
@@ -1,43 +1,7 @@
 <template>
   <h1>Home</h1>
-  <v-chip color="primary"> Primary </v-chip>
-  <v-container>
-    <v-row align="center" justify="center">
-      <v-col cols="auto">
-        <v-btn density="compact" icon="mdi-plus"></v-btn>
-      </v-col>
+  <v-chip color="primary"> ça marche </v-chip>
 
-      <v-col cols="auto">
-        <v-btn density="comfortable" icon="$vuetify"></v-btn>
-      </v-col>
-
-      <v-col cols="auto">
-        <v-btn density="default" icon="mdi-open-in-new"></v-btn>
-      </v-col>
-    </v-row>
-
-    <v-row align="center" justify="center">
-      <v-col cols="auto">
-        <v-btn icon="mdi-account" size="x-small"></v-btn>
-      </v-col>
-
-      <v-col cols="auto">
-        <v-btn icon="mdi-plus" size="small"></v-btn>
-      </v-col>
-
-      <v-col cols="auto">
-        <v-btn icon="$vuetify"></v-btn>
-      </v-col>
-
-      <v-col cols="auto">
-        <v-btn icon="mdi-open-in-new" size="large"></v-btn>
-      </v-col>
-
-      <v-col cols="auto">
-        <v-btn icon="mdi-calendar" size="x-large"></v-btn>
-      </v-col>
-    </v-row>
-  </v-container>
   <v-select
     label="Select"
     :items="['California', 'Colorado', 'Florida', 'Georgia', 'Texas', 'Wyoming']"
