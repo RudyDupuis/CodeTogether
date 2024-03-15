49ae30b9255aa67d263973db5ad57f50cfb6d198
RudyDupuis <abcdupuisrudy@gmail.com>
2024-03-15 20:52:17 +0100

    Install vuetify

--- Front/src/main.ts
+++ Front/src/main.ts
@@ -1,11 +1,22 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+
 import router from './router'
+
+import { createVuetify } from 'vuetify'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/dist/vuetify.min.css'
 
 const app = createApp(App)
 
 app.use(router)
+
+app.use(
+  createVuetify({
+    theme: {
+      defaultTheme: 'dark'
+    }
+  })
+)
 
 app.mount('#app')
