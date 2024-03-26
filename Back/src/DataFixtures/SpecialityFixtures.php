7a8ee7153d3ad39e1b6c56b955eed39b402a08cb
Jordan Robin <126224637+Jordan-Robin@users.noreply.github.com>
2024-03-26 11:10:11 +0100

    add technologyFixtures and specialityFixtures (#43)
    * Ajout fixtures : 10 technologies et 5 spécialités
    
    * Prise en compte des modification
    
    * Delete Back/composer.lock
    
    * readd composer.lock
    
    * dowgrade composer.lock
    
    ---------
    
    Co-authored-by: Rudy Dupuis <106442315+RudyDupuis@users.noreply.github.com>
    Co-authored-by: RudyDupuis <abcdupuisrudy@gmail.com>

---
+++ Back/src/DataFixtures/SpecialityFixtures.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Speciality;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class SpecialityFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $specialitiesList = [
+            'Front-end developer',
+            'Back-end developer',
+            'Full-stack developer',
+            'Designer',
+            'DevOps',
+        ];
+
+        for ($i = 0; $i < 5; ++$i) {
+            $speciality = new Speciality();
+            $speciality->setLabel($specialitiesList[$i]);
+            $manager->persist($speciality);
+        }
+
+        $manager->flush();
+    }
+}
---
+++ Back/src/DataFixtures/TechnologyFixtures.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Technology;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class TechnologyFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $technologiesList = [
+            'Javascript',
+            'React',
+            'Angular',
+            'Java',
+            'PHP',
+            'Symfony',
+            'SQL',
+            'Python',
+            'HTML',
+            'CSS',
+        ];
+
+        for ($i = 0; $i < 10; ++$i) {
+            $technology = new Technology();
+            $technology->setLabel($technologiesList[$i]);
+            $manager->persist($technology);
+        }
+
+        $manager->flush();
+    }
+}
