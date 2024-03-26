bd045c75bb54997b01e4fbbebb36d704112e8709
JordanRobin <jordanrobin100@gmail.com>
2024-03-26 09:55:06 +0100

    Prise en compte des modification

--- Back/src/DataFixtures/UserFixtures.php
+++ Back/src/DataFixtures/UserFixtures.php
@@ -2,8 +2,6 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Speciality;
-use App\Entity\Technology;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -12,9 +10,6 @@ class UserFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-
-        //Users
-
         $user1 = new User();
         $user1->setEmail('john.doe@example.com');
         $user1->setRoles(['ROLE_USER']);
@@ -32,45 +27,6 @@ class UserFixtures extends Fixture
         $user3->setRoles(['ROLE_USER']);
         $user3->setPassword('password');
         $manager->persist($user3);
-
-
-        //Technologies
-
-        $technologiesList = [
-            "Javascript",
-            "React",
-            "Angular",
-            "Java",
-            "PHP",
-            "Symfony",
-            "SQL",
-            "Python",
-            "HTML",
-            "CSS"
-        ];
-
-        for ( $i = 0; $i <10; $i++ ) {
-            $technology = new Technology();
-            $technology->setLabel( $technologiesList[$i] );
-            $manager->persist( $technology );
-        }
-
-
-        //Specialities
-
-        $specialitiesList = [
-            "Front-end developer",
-            "Back-end developer",
-            "Full-stack developer",
-            "Designer",
-            "DevOps"
-        ];
-
-        for ( $i = 0; $i <5; $i++ ) {
-            $speciality = new Speciality();
-            $speciality->setLabel( $specialitiesList[$i] );
-            $manager->persist( $speciality );
-        }
 
         $manager->flush();
     }
