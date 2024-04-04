1cde0261d7ef14cd50bd95699ea1bcb9c636c6cb
JordanRobin <jordanrobin100@gmail.com>
2024-04-04 22:22:36 +0200

    Petite modif pour affichage trait de délimitation blanc en bas du header

--- Front/src/components/header/CTHeader.vue
+++ Front/src/components/header/CTHeader.vue
@@ -4,7 +4,7 @@ import PictoProfile from '@/components/svg/PictoProfile.vue'
 </script>
 
 <template>
-  <header class="f f-row j-betw a-cent p-8-16" id="globalHeader">
+  <header class="f f-row j-betw a-cent p-8-16">
     <router-link :to="{ name: 'home' }">
       <CTLogo />
     </router-link>
@@ -25,7 +25,7 @@ import PictoProfile from '@/components/svg/PictoProfile.vue'
 </template>
 
 <style scoped>
-#globalHeader {
+header {
   border-bottom: 1px solid rgba(255, 255, 255, 0.5);
 }
 
